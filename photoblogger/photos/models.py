3d3332518bb82f18276b21411c246c5fcb12adda
Joonas <joonas.heinonen@edu.turkuamk.fi>
2019-12-31 00:44:05 +0200

    Second commit.

--- photoblogger/photos/models.py
+++ photoblogger/photos/models.py
@@ -1,3 +1,14 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    tagline  = models.CharField(max_length=200, default="Lorem ipsum")
+    content  = models.CharField(max_length=4000)
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.content
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- photoblogger/photos/views.py
+++ photoblogger/photos/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Post
 
 def index(request):
-    return HttpResponse("<h1>Turveteloa!</h1>")
+    latest_post_list = Post.objects.order_by('-pub_date')[:5]
+    context = {'latest_post_list': latest_post_list,}
+    return render(request, 'photos/index.html', context)
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'photos/detail.html', {'post': post})
+
+def results(request, post_id):
+    response = "You are looking at the results of post %s."
+    return HttpResponse(response % post_id)
+
+def like(request, post_id):
+    return HttpResponse("You're liking/disliking %s." % post_id)
